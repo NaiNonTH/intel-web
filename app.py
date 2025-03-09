ec051cba862bc82ca5b2f3c3b449115bd5078793
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2025-03-10 00:31:02 +0700

    Add more details to the homepage

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ def main():
     st.write(
         """
         # Hello, world
-        This is my first time on Streamlit
+
+        This is a web app project for Intelligence System. Pages are located on the sidebar to your left (or click an arrow button on the top-left corner if you don't see one).
+
+        **จัดทำโดย**
+        นายพงศภัค ดวงมณี (6604062630331)
+
+        This web app consists of four pages:
+        * Machine Learning model introduction
+        * Neural Network introduction
+        * Machine Learning model demo
+        * Neural Network demo
         """)
 
 if __name__ == "__main__":
